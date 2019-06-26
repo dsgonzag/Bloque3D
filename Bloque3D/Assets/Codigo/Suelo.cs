229ab3041edf5af2306c4973c9c7e5a6880c13a1
dsgonzag <45009733+dsgonzag@users.noreply.github.com>
2019-06-26 01:54:16 -0500

    Añadir sistema de vidas

---
+++ Bloque3D/Assets/Codigo/Suelo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Suelo : MonoBehaviour
+{
+    public Vidas vidas;
+
+
+    void OnTriggerEnter()
+    {
+        vidas.PerderVida();
+    }
+
+    
+}
---
+++ Bloque3D/Assets/Codigo/Vidas.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+public class Vidas : MonoBehaviour
+{
+
+    public static int vidas = 3;
+    public Text textoVidas;
+
+    public pelota pelota;
+    public Barra barra;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ActualizarMarcadorVidas();
+            
+    }
+
+    void ActualizarMarcadorVidas()
+    {
+        textoVidas.text = "Vidas: " + Vidas.vidas;
+    }
+   public void PerderVida()
+    {
+        Vidas.vidas--;
+        ActualizarMarcadorVidas();
+
+        barra.Reset();
+        pelota.Reset();
+    }
+}
