9bed6d9b8b6518777edd2774b40a0c4e98130d82
dsgonzag <45009733+dsgonzag@users.noreply.github.com>
2019-06-30 00:27:05 -0500

    Agregando menu

---
+++ Bloque3D/Assets/Codigo/SonidoFinPartida.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SonidoFinPartida : MonoBehaviour
+{
+
+    public AudioSource audioSource;
+    public AudioClip completado;
+    public AudioClip gameOver;
+    // Start is called before the first frame update
+ 
+    public void GameOver()
+    {
+        ReproduceSonido(gameOver);
+    }
+    public void NivelCompletado()
+    {
+        ReproduceSonido(completado);
+    }
+    void ReproduceSonido(AudioClip sonido)
+    {
+        audioSource.clip = sonido;
+        audioSource.loop = false;
+        audioSource.Play();
+    }
+}
---
+++ Bloque3D/Assets/Codigo/SonidosPelota.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SonidosPelota : MonoBehaviour
+{
+    public AudioSource rebote;
+    public AudioSource punto;
+    // Start is called before the first frame update
+
+
+    void OnCollisionEnter(Collision otro)
+    {
+       if(otro.gameObject.CompareTag("Bloques"))
+        {
+            punto.Play();
+        }
+        else{
+            rebote.Play();
+        }
+    }
+}
